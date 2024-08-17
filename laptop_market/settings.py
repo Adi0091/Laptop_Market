a33643994de7af6fcc8da3d4ba7a5096bb38d18d
Adi0091 <waghayeadi@gmail.com>
2024-08-17 15:05:32 +0530

    small Update

--- laptop_market/settings.py
+++ laptop_market/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_=*+(=zkvpx@fk2ljvqui)c=9i&efk*y$-y%_j+2woghx7g7+'
+with open('C:\Users\Admin\Desktop\Laptop_market\secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
